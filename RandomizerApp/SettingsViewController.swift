1ece95bd505fe3bcd42a9587e47d7d3fabe07591
Konstantin <simussev.k@icloud.com>
2022-06-22 13:04:43 +0500

    Setup main storyboard

---
+++ RandomizerApp/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  RandomizerApp
+//
+//  Created by Konstantin Simusev on 22.06.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
