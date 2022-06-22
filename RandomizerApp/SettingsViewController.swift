e59c960e681ce5151a6bd33c0948e1d0cb54a34a
Konstantin <simussev.k@icloud.com>
2022-06-22 15:45:01 +0500

    Work with logic

--- RandomizerApp/SettingsViewController.swift
+++ RandomizerApp/SettingsViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    @IBOutlet var minimumValueTF: UITextField!
+    @IBOutlet var maximumValueTF: UITextField!
+    
+    var minimumValue: String!
+    var maximumValue: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        minimumValueTF.text = minimumValue
+        maximumValueTF.text = maximumValue
     }
     
+    @IBAction func cancelAction() {
+        dismiss(animated: true)
+    }
 }
